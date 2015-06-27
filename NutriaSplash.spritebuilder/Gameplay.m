a2de75f0ade2eb39e27bae0428fbda383277fd67
Esteban <ep28469@gmail.com>
2015-06-27 12:29:48 -0500

    Gameplay created

---
+++ NutriaSplash.spritebuilder/Gameplay.m
@@ -0,0 +1,25 @@
+//
+//  Gameplay.m
+//  NutriaSplash
+//
+//  Created by Esteban Piazza Vázquez on 27/06/15.
+//  Copyright 2015 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+
+@implementation Gameplay
+
+
+-(void)didLoadFromCCB{
+    
+}
+
+-(void)onEnter{
+    [super onEnter];
+}
+
+
+
+@end
--- NutriaSplash.spritebuilder/Source/MainScene.m
+++ NutriaSplash.spritebuilder/Source/MainScene.m
@@ -1,5 +1,14 @@
 #import "MainScene.h"
 
-@implementation MainScene
+@implementation MainScene{
+    CCButton* _playButton;
+    
+}
+
+-(void)play {
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
+    _playButton.enabled = false;
+}
 
 @end
